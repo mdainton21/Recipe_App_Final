c3ba19652648d4a36690a83b24b0ba3552dd8757
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2024-08-03 18:27:38 -0400

    Final Version

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'recipes/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
