5d8358c6b8eafb6a278d42561743793b39e7c7ac
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2024-08-03 13:00:28 -0400

    Fixed Procfile

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ectv@5=f=y&-qj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
