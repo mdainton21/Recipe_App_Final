9785c6644545a07a99b3e29fa1831e9d7ad58564
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2024-08-02 23:51:12 -0400

    Updated requirements.txt

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-ectv@5=f=y&-qj$3lb^_choc%z6+^bk8azk6#fwv=b--@_8ijc')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ectv@5=f=y&-qj$3lb^_choc%z6+^bk8azk6#fwv=b--@_8ijc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
